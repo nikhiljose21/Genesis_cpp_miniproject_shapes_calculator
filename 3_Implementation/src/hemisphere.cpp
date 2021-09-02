ec02fd95a523272fdf20d8f7f0816fc0d9d23837
RAkanksha <80452069+RAkanksha@users.noreply.github.com>
2021-09-02 20:34:31 +0530

    Uploading pentagon and hemisphere cpp files

---
+++ 3_Implementation/src/hemisphere.cpp
@@ -0,0 +1,34 @@
+#include"shape.h"
+#include<math.h>
+        
+    hemisphere::hemisphere():radius(0){}
+
+    hemisphere::hemisphere(double r):radius(r){}
+    bool hemisphere::valid_input()
+        {
+            if(radius == 0.0){
+                return false;
+                }
+            else if(radius<0){
+                radius = fabs(radius);
+                return true;
+            }
+            else return true;
+            
+        }
+    void hemisphere::display()
+        {
+            std::cout<<"volume="<<volume()<<" "<<"Lateral surface area="<<LSA()<<" "<<"Total surface area="<<TSA()<<"\n";
+        }
+    double hemisphere::volume()
+        {
+            return (0.666)*pow(radius,3)*22/7;
+        }
+    double hemisphere::LSA()
+        {
+            return 2*pow(radius,2)*22/7;
+        }
+    double hemisphere::TSA()
+        {
+            return 3*pow(radius,2)*22/7;
+        }
