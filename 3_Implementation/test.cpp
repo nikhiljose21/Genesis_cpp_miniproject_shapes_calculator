3c55e21b70a2a14ddb588e897b8c9b3ab68f3d20
258157 <80401577+GanigaPreethu@users.noreply.github.com>
2021-09-05 21:39:49 +0530

    Update test.cpp

--- 3_Implementation/test.cpp
+++ 3_Implementation/test.cpp
@@ -32,5 +32,41 @@ TEST(hemisphere,Test_pentagon_functions) {
   EXPECT_FLOAT_EQ(339.42856, h.TSA());
 }
 
+  TEST(hexagon,Test_hexagon_Valid_inputs) {
+    hexagon h(3.0);
+    EXPECT_TRUE(h.valid_input()); 
+    hexagon h1(0.0);
+    EXPECT_FALSE(h1.valid_input()); 
+    hexagon h2(-3.0);
+    EXPECT_TRUE(h2.valid_input());
+}
+    
+    
+    TEST(hexagon,Testing_functions)
+ {
+    hexagon h(3.0);
+  EXPECT_FLOAT_EQ(18, h.perimeter());
+  EXPECT_FLOAT_EQ(23.382, h.area());
+}
+
+
+TEST(octagon,Test_octagon_Valid_inputs) {
+    octagon o(3.0);
+    EXPECT_TRUE(o.valid_input()); 
+    octagon o1(0.0);
+    EXPECT_FALSE(o1.valid_input()); 
+    octagon o2(-3.0);
+    EXPECT_TRUE(o2.valid_input());
+}
+    
+    
+TEST(octagon,Testing_functions)
+ {
+    octagon o(3.0);
+  EXPECT_FLOAT_EQ(24, o.perimeter());
+  EXPECT_FLOAT_EQ(43.4556, o.area());
+}
+
+
 
 }
