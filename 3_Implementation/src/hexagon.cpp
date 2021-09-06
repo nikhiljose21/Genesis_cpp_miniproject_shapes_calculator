eb3fae1ff19a1e3cbff18aef9aff4dc5a09c5744
258157 <80401577+GanigaPreethu@users.noreply.github.com>
2021-09-06 16:14:50 +0530

    Updated documentation

--- 3_Implementation/src/hexagon.cpp
+++ 3_Implementation/src/hexagon.cpp
@@ -1,18 +1,36 @@
 #include"shape.h"
   #include<math.h>
+
+/**
+ * @brief default constructor for hexagon
+ * 
+ */
     hexagon::hexagon()
     {
         side=0;
         interior_angle=0;
         exterior_angle=0;
     }
-        //parameterised constructor
+        
+/**
+ * @brief parameterised constructor for hexagon
+ * 
+ * @param s
+ */
+
     hexagon::hexagon(double s)
     {
         side=s;
         interior_angle=120;
         exterior_angle=60;
     }
+
+/**
+ * @brief function to check if the input is valid or not
+ * 
+ * @return true if the inputs are valid
+ * @return false if the inputs are invalid
+ */
     bool hexagon::valid_input()
         {
             if(side==0.0){
@@ -24,18 +42,38 @@
             }
             else return true;
         }
+
+/**
+ * @brief function to calculate the perimeter of hexagon
+ * 
+ * @return double perimeter
+ */
+    double hexagon::perimeter()
+        {
+            return 6*side;
+        }
+
+
+/**
+ * @brief function to calculate area of hexagon
+ * 
+ * @return double area 
+ */
+    double hexagon::area()
+        {
+            return 2.598*pow(side,2);
+        }
+
+
+/**
+ * @brief function to display area, perimeter, interior and exterior angle of hexagon
+ * 
+ */
+
     void hexagon::display()
         {
     std::cout<<"Perimeter of hexagon:"<<perimeter()<<"\n";
     std::cout<<"Area of hexagon:"<<area()<<"\n"; 
     std::cout<<"Interior angle(in degrees) :"<<interior_angle<<"\n";
     std::cout<<"Exterior angle(in degrees) : "<<exterior_angle<<"\n";  
-        }
-    double hexagon::perimeter()
-        {
-            return 6*side;
-        }
-    double hexagon::area()
-        {
-            return 2.598*pow(side,2);
         }
