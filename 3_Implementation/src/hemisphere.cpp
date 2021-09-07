b689ead57b2a9104d4e9f81b1a8f91ab004bcbf7
RAkanksha <80452069+RAkanksha@users.noreply.github.com>
2021-09-07 12:23:39 +0530

    Update documentation

--- 3_Implementation/src/hemisphere.cpp
+++ 3_Implementation/src/hemisphere.cpp
@@ -1,9 +1,22 @@
 #include"shape.h"
 #include<math.h>
-        
+    /**
+     * @brief default constructor
+     * 
+     */
     hemisphere::hemisphere():radius(0){}
-
+    /**
+     * @brief parameterised constructor for hemisphere
+     * 
+     * @param r radius value
+     */
     hemisphere::hemisphere(double r):radius(r){}
+    /**
+     * @brief Testing the input
+     * 
+     * @return true 
+     * @return false 
+     */
     bool hemisphere::valid_input()
         {
             if(radius == 0.0){
@@ -16,18 +29,37 @@
             else return true;
             
         }
+    /**
+     * @brief Displaying the parameters
+     * 
+     */
     void hemisphere::display()
         {
-            std::cout<<"Volume="<<volume()<<"\n"<<"Lateral surface area="<<LSA()<<"\n"<<"Total surface area="<<TSA()<<"\n";
+            std::cout<<"volume="<<volume()<<" "<<"Lateral surface area="<<LSA()<<" "<<"Total surface area="<<TSA()<<"\n";
         }
+    /**
+     * @brief Calculating volume value
+     * 
+     * @return double volume value
+     */
     double hemisphere::volume()
         {
             return (0.666)*pow(radius,3)*22/7;
         }
+    /**
+     * @brief Calculating lateral surface area value
+     * 
+     * @return double lateral surface area value
+     */
     double hemisphere::LSA()
         {
             return 2*pow(radius,2)*22/7;
         }
+    /**
+     * @brief Calculating total surface area value
+     * 
+     * @return double total surface area value
+     */
     double hemisphere::TSA()
         {
             return 3*pow(radius,2)*22/7;
